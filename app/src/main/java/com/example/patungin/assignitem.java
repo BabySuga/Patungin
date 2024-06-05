806159c8add93a5de43b41043460e8872b5f8175
BabySuga <eguchigamingart@gmail.com>
2024-06-05 14:10:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/patungin/assignitem.java
@@ -0,0 +1,14 @@
+package com.example.utsassignitem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class assignitem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignitem);
+    }
+}
